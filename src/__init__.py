dc89570574839a6b8eacb5757f9a1e670ef36431
Jonathan Zea <jonathan.a.zea@ieee.org>
2024-08-13 18:47:22 -0500

    :white_check_mark: upload ODE code

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,5 @@ from .linear_syst_methods import eliminacion_gaussiana, descomposicion_LU, resol
 from .min_cuadrados import ajustar_min_cuadrados  # type: ignore
 
 from .iterative_methods import gauss_jacobi, gauss_seidel  # type: ignore
+
+from .ODE import ODE_euler, ODE_euler_nth  # type: ignore
