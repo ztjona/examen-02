a32f4a67ac75ca1eee79a9a6d40a9880ed43f758
Jonathan Zea <jonathan.a.zea@ieee.org>
2024-08-13 09:45:04 -0500

    :tada: upload code for exam

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .linear_syst_methods import eliminacion_gaussiana, descomposicion_LU, resolver_LU, matriz_aumentada, separar_m_aumentada, gauss_jordan  # type: ignore
+from .min_cuadrados import ajustar_min_cuadrados  # type: ignore
+
+from .iterative_methods import gauss_jacobi, gauss_seidel  # type: ignore
